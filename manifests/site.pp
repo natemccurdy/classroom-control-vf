c50b4e4044dcecc33f16cf9b043ec01c0f2c17df
scorchescarcha <x3open@yahoo.com>
2017-03-14 12:43:53 +0800

    put hello content in motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,11 +36,17 @@ ini_setting { 'random ordering':
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
+# specified in the console for that nod
 
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  file { '/etc/motd';
+  esure => preset,
+  content => 'Hello.'
+  
+  }
 }
