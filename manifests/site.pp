119bbf5f5c26fcb3e17cb214e70decff78a9c8d8
Nate McCurdy <nate.mccurdy@puppet.com>
2017-03-14 15:48:54 -0700

    Write motd using cowsay :)

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,12 +44,9 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Welcome to Nate's super awesome node!\n",
+  exec { 'cowsay motd':
+    command => "cowsay 'Welcome to ${::fqdn}' > /etc/motd",
+    creates => '/etc/motd',
   }
   
 }
