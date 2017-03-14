7b53f4946003b7778b2044808ddd0b65482d694a
scorchescarcha <x3open@yahoo.com>
2017-03-14 12:49:53 +0800

    updated file title

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd';
+  file { '/etc/motd':
   esure => preset,
   content  => "Testing motd entry./n",
   
