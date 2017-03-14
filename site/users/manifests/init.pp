22b8f41b24dda18ae3bfe3819de614a8ae658c7c
scorchescarcha <x3open@yahoo.com>
2017-03-15 07:33:26 +0800

    set

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,3 +1,10 @@
 user { 'fundamentals':
-ensure => present,
-}
+  ensure => present,
+  gid              => '10',
+  home             => '/home/fudamentals',
+  password         => '!!',
+  password_max_age => '99999',
+  password_min_age => '0',
+  shell            => '/bin/bash',
+  uid              => '1002',
+ }
