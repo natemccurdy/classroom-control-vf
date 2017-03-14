d7f19af9410b4fca9f2cf93adf3d9bf91d4efb75
Justin Duck <jdpuppet@mailinator.com>
2017-03-14 15:54:39 +1100

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,14 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+
+	file { '/etc/motd':
+	  ensure  => file,
+	  owner   => 'root',
+	  group   => 'root',
+	  mode    => '0644',
+	  content => "Exercise 7.1\n",
+	}
+
 }
+
