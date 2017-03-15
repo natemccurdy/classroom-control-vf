67bd033f21c1b508d7e9f528b4619e540aab02e8
scorchescarcha <x3open@yahoo.com>
2017-03-15 12:24:15 +0800

    checked

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -3,7 +3,7 @@ class skeleton {
 File {
       owner => 'root',
       group => 'root',
-}
+      }
 
 file {  '/etc/skel':
          ensure => directory   
@@ -14,6 +14,6 @@ file { '/etc/skel/.bashrc':
        ensure => file,
        mode => '0644',
        source => 'puppet:///modules/skeleton/mybashrc',
-}
+     }
 
 }
