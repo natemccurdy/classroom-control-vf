aaa962321eb7217ec82a42ca2084cd13bb6381ac
Justin Duck <jdpuppet@mailinator.com>
2017-03-15 15:33:17 +1100

    Refactored nginx init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,46 @@
+class nginx {
+
+  $docroot    = '/var/www'
+  $confdir    = '/etc/nginx'
+  $conf_d_dir = "${confdir}/conf.d"
+  
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+  
+  package { 'nginx':
+    ensure => present,
+  }
+
+  file { $docroot:
+    ensure => directory,
+  }
+
+  file { "${docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file { "${confdir}/nginx.conf":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  file { "${conf_d_dir}/default.conf":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+
+}
+
