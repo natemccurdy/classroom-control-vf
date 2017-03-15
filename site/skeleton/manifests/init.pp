3077bbc4a25a69ee20a250abb3bc45ba5697d72e
birendersingh03 <birendersingh03@gmail.com>
2017-03-15 06:06:35 +0530

    update1

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,7 +1,20 @@
 class skeleton {
 
+file { 'skel':
+ensure => 'directory',
+path => '/etc/skel/',
+owner => 'root',
+group => 'root'
+mode => '0755'
+}
 
-
+file { 'bashrc':
+ensure => 'file',
+path => '/etc/skel/.bashrc',
+owner => 'root',
+group => 'root',
+ mode => '0644',
+}
 
 
 
