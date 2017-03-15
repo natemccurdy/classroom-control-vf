cc73a425ebcb43ce2ddf5fd98af2f84a3d3d0695
cbustama <crystal.bustamante@macquarie.com>
2017-03-15 08:28:16 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,7 +2,7 @@ class skeleton {
   file { '/etc/skel':
     ensure => directory,
     }
-  file { '/etc/skel/.bashrc'
+  file { '/etc/skel/.bashrc':
     ensure => file,
     source => 'puppet:///modules/skeleton/.bashrc',
     }
