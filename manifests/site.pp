17ef25041e51b54864cc12a014afe1208eced78a
macborje <mac.borje@gmail.com>
2017-03-15 10:59:25 +0800

    include memcached

--- manifests/site.pp
+++ manifests/site.pp
@@ -54,6 +54,7 @@ node default {
 #}
 include users
 include skeleton
+include memcached
 exec { "cowsay 'Welcome to ${::fqdn}! mac is so cool' > /etc/motd":
 path => '/usr/bin:/usr/local/bin',
 creates => '/etc/motd',
