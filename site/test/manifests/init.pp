5ef9f424a580bf4d589c8049a92e3520d0df016a
birendersingh03 <birendersingh03@gmail.com>
2017-03-15 06:43:27 +0530

    update3

--- site/test/manifests/init.pp
+++ site/test/manifests/init.pp
@@ -1,13 +1,12 @@
 class test {
 
 file {'testmodule':
-
 ensure => file,
 path => '/etc/test.txt',
 owner => 'root',
 group => 'root',
-mode => '0644'
-source => puppet://modules/test/abc
+mode => '0644',
+source => 'puppet://modules/test/abc',
 
 }
 
