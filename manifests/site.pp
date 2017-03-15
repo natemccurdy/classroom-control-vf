c40e09e781ddf5b18e0f81b8ce5c8f26a1d78b3a
Nate McCurdy <nate.mccurdy@puppet.com>
2017-03-14 19:05:56 -0700

    Play with node definitions

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,15 +28,19 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
 
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
+
+
+node 'natemccurdy.puppetlabs.vm' {
+  notify { 'This came from the specific node definition': }
+}
+
+node /natemccurdy/ {
+  notify { 'This came from the regex node definnition': }
+}
+
+
+
 
 node default {
   # This is where you can declare classes for all nodes.
