54f85cfc0a8d5fa857d40d8d566824d70536fbc7
birendersingh03 <birendersingh03@gmail.com>
2017-03-15 06:10:36 +0530

    update3

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -4,8 +4,8 @@ file { 'skel':
 ensure => 'directory',
 path => '/etc/skel/',
 owner => 'root',
-group => 'root'
-mode => '0755'
+group => 'root',
+mode => '0755',
 }
 
 file { 'bashrc':
@@ -14,7 +14,7 @@ path => '/etc/skel/.bashrc',
 owner => 'root',
 group => 'root',
  mode => '0644',
- source => 'puppet:///modules/skeleton/bashrc'
+ source => 'puppet:///modules/skeleton/.bashrc',
 }
 
 
