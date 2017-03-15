3035c592a3d88067f66daf936e9e3d2cdb1c6d1a
mpofernandez <mpofernandez@gmail.com>
2017-03-16 06:25:21 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -61,9 +61,12 @@ creates => '/etc/motd',
 #include skeleton
 #include memcached
 
-if $::virtual != 'physical' {
-$vmname = capitalize($::virtual)
+if $virtual  {
+$vmname = capitalize($virtual)
 notify { "This is a ${vmname} virtual machine.": }
+elsif $physical {
+notify { "This is a ${physical} machine.": }
+}
 }
 
 }
