d4f3d992251266310b10bc49c2502bb2cd5e5a22
cbustama <crystal.bustamante@macquarie.com>
2017-03-15 08:37:26 +0800

    update requires

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,24 +1,28 @@
 class nginx {
 service { 'nginx':
   ensure => running,
-  require => [Package['nginx'], File['/var/www'], File['/var/www/index.html'], File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/default.conf']],
+  require => [File['/var/www/index.html'], File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/default.conf']],
   }
 package { 'nginx':
   ensure => installed,
   }
 file { '/var/www' :
   ensure => directory,
+  require => Package['nginx'],
   }
 file { '/var/www/index.html' :
   ensure => file,
-  source => 'puppet:///modules/nginx/index.html'
+  source => 'puppet:///modules/nginx/index.html',  
+  require => File['/var/www'],
   }
 file { '/etc/nginx/nginx.conf':
   ensure => file,
   source => 'puppet:///modules/nginx/nginx.conf'
+  require => Package['nginx'],
   }
 file { '/etc/nginx/conf.d/default.conf':
   ensure => file,
   source => 'puppet:///modules/nginx/default.conf'
+  require => Package['nginx'],
   }
  }
