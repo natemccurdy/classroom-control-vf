76170a13a43d00e9d65ccb9c6284011ebf0fde97
cbustama <crystal.bustamante@macquarie.com>
2017-03-15 10:49:50 +0800

    create memcached module

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,14 @@
+class memcached {
+  package { 'memcached' :
+    ensure => installed,
+    }
+  file { '/etc/sysconfig/memcached' :
+    ensure => file,
+    source => 'puppet:///modules/memcached/memcached',  
+    require => Package['memcached'],
+  }
+  service { 'memcached' :
+    ensure => running,
+    subscribe => file['/etc/sysconfig/memcached'],
+  }
+}
