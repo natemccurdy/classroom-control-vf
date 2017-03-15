5807b2d4b7e5745e4c28a9613fc91e487b42a07c
cbustama <crystal.bustamante@macquarie.com>
2017-03-15 13:12:59 +0800

    Add debian

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,6 +11,12 @@ $module_dir = 'puppet:///modules/nginx'
 $web_dir = '/var/www'
 $nginx_conf_dir = '/etc/nginx'
 
+case $facts['os']['name'] {
+  'Debian':  { $user = epp('nginx/nginx.epp', { user => 'www-data' })} 
+  'RedHat':  { $user = epp('nginx/nginx.epp', { user => 'nginx' })} 
+  default :  { notice("OS not supported"); $user = epp('nginx/nginx.epp', { user => 'nginx' }) }
+}
+
 service { 'nginx':
   ensure => running,
   require => [File['/var/www/index.html'], File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/default.conf']],
@@ -27,7 +33,7 @@ file { "${web_dir}/index.html" :
   }
 file { "${nginx_conf_dir}/nginx.conf" :
   ensure => file,
-  source => "${module_dir}/nginx.conf",
+  source => $user,
   }
 file { "${nginx_conf_dir}/conf.d/default.conf":
   ensure => file,
