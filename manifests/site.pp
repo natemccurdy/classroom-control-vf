c6757be3849a226d6725eb5fc2ade48f2c6dcf32
cbustama <crystal.bustamante@macquarie.com>
2017-03-16 07:11:12 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  include Class['nginx','users','skeleton','memcached']
+  include Class['nginx','users','skeleton','memcached','aliases']
 
   
 
