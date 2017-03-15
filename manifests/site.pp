e27a4c36376dd90816bcb7917f2a207c78982b40
mpofernandez <mpofernandez@gmail.com>
2017-03-16 06:20:23 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,12 @@ path => '/usr/bin:/usr/local/bin',
 creates => '/etc/motd',
 }
 
-include skeleton
-include memcached
+#include skeleton
+#include memcached
+
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+
 }
