85f15a1f6c13a2519cb9813c7ad7559e654ccf5a
vraw1 <xxxdtuyyy@gmail.com>
2017-03-15 08:23:37 +0530

    updating content

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -7,6 +7,11 @@ package { 'memcached':
   file { '/etc/sysconfig/memcached':
     ensure  => file,
     require => Package['memcached'],
+    content => 'PORT="11211"
+USER="memcached"
+MAXCONN="96"
+CACHESIZE="32"
+OPTIONS=""',
   }
   
   service { 'memcached':
