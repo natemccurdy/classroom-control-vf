6dbf112ca2341f42f970a5043b748beb66b3ea68
Justin Duck <jdpuppet@mailinator.com>
2017-03-15 13:58:37 +1100

    Managing memcached package/file/service

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -12,7 +12,7 @@ class memcached {
   service { 'memcached':
     ensure => running,
     enable => true,
-    subscribe => File['/etc/sysconfig/memcahced'],
+    subscribe => File['/etc/sysconfig/memcached'],
   }
 
 }
