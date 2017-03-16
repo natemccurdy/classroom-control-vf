dfe3d5a2ce28bc7565e6206908fbd9a42059cd6c
Justin Duck <jdpuppet@mailinator.com>
2017-03-16 11:38:38 +1100

    fixed typo

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,9 +1,9 @@
 class users::admins {
 
-$users = ['justin', 'alice', 'bob', 'cee',]
-
-  $users.each |$user| {
-    users::user_defined { $user: }
+$users = [ 'justin', 'alice', 'bob', 'cee', ]
+ 
+$users.each |$user| {
+   users::user_defined { $user: }
 
 }
 
