02893d4daaad6d8890a0aea47f41b55b0f6a9458
cbustama <crystal.bustamante@macquarie.com>
2017-03-16 11:10:22 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,58 +1,18 @@
 class nginx (
-  $root = '/var/www'
-)
+  $package_name = $nginx::params::package_name ,
+  $owner = $nginx::params::owner ,
+  $group= $nginx::params::group ,
+  $web_dir = $nginx::params::web_dir ,
+  $nginx_conf_dir = $nginx::params::nginx_conf_dir ,
+  $nginx_block_dir = $nginx::params::nginx_block_dir ,
+  $log_dir = $nginx::params::log_dir ,
+  $block_file = $nginx::params::block_file ,
+) inherits nginx::params
 {
 
 $module_dir = 'puppet:///modules/nginx'
 
-case $facts['os']['name'] {
-  'Debian','RedHat','CentOS' :  { 
-    $package_name = 'nginx'; 
-    $owner = 'root'; 
-    $group= 'root'; 
-    $web_dir = $root; 
-    $nginx_conf_dir = '/etc/nginx';
-    $nginx_block_dir = '/etc/nginx/conf.d';
-    $log_dir = '/var/log/nginx';
-    $block_file = epp('nginx/default.epp', { web_dir => $web_dir })
-    } 
-  'Windows':  { 
-    $package_name = 'nginx-service';
-    $owner = 'Administrator'; 
-    $group= 'Administrators'; 
-    $web_dir = $root; 
-    $nginx_conf_dir = 'C:/ProgramData/nginx';
-    $nginx_block_dir = 'C:/ProgramData/nginx/conf.d';
-    $log_dir = 'C:/ProgramData/nginx/logs'
-    $block_file = epp('nginx/default.epp', { web_dir => $web_dir })
-    } 
-   default :  { notice("OS not supported") }
-}
 
-case $facts['os']['name'] {
-  'Debian':  { $nginx_conf = epp('nginx/nginx.epp', { 
-    user => 'www-data', 
-    web_dir => $web_dir, 
-    nginx_conf_dir => $nginx_conf_dir, 
-    nginx_block_dir => $nginx_block_dir,
-    log_dir => $log_dir
-    })} 
-  'RedHat','CentOS':  { $nginx_conf = epp('nginx/nginx.epp', { 
-    user => 'nginx', 
-    web_dir => $web_dir, 
-    nginx_conf_dir => $nginx_conf_dir, 
-    nginx_block_dir => $nginx_block_dir,
-    log_dir => $log_dir
-    })} 
-  'Windows':  { $nginx_conf = epp('nginx/nginx.epp', { 
-    user => 'nobody', 
-    web_dir => $web_dir, 
-    nginx_conf_dir => $nginx_conf_dir, 
-    nginx_block_dir => $nginx_block_dir,
-    log_dir => $log_dir
-    })} 
-   default :  { notice("OS not supported") }
-}
 
 File {
 owner => $owner,
