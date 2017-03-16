a36a889cb084735d1470e47fb9172d92b7f93c5a
Nate McCurdy <nate.mccurdy@puppet.com>
2017-03-15 21:48:26 -0700

    fix things

--- site/wrappers/manifests/redis_example.pp
+++ site/wrappers/manifests/redis_example.pp
@@ -1,16 +1,15 @@
 class wrappers::redis_example {
 
- # Redis requires EPEL
- class { 'epel':
-   before => Class['redis', 'selinux'],
- }
- 
- include redis
- 
- # Lock down this redis box with selinux
- class { 'selinux':
-   mode           => 'enforcing',
-   manage_package => false,
- }
+  # Redis requires EPEL
+  class { 'epel':
+    before => Class['redis', 'selinux'],
+  }
+
+  include redis
+
+  # Lock down this redis box with selinux
+  class { 'selinux':
+    mode => 'enforcing',
+  }
 
 }
