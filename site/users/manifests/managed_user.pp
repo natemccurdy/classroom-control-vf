f2a4f00a4de6427af85407eb15d05bb5cec79fca
scorchescarcha <x3open@yahoo.com>
2017-03-16 08:27:43 +0800

    updated with home dir/ssh keyes

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,16 +1,37 @@
 # defined type
-define users::managed_user {
+define users::managed_user (
                              $username =$title,
-                             $homedir ="/home/$(username)",
-                            }     
+                             $homedir ="/home/${username}",
+                            )     
 # parameters
 {
   user { $username:
-         ensure => preset,
-         gid => $username,
-       }
-   group { $groupname:
-          ensure => present,
-         }
-
+    ensure => preset,
+    gid => $username,
+  }
+  
+  group { $groupname:
+    ensure => present,
+  }
+  
+  file { $homedir:
+    ensure => directory,
+    owner  => $username,
+    group  => $groupname,
+    mode   => '0750',
+  }
+  
+  file { "${homedir}/.ssh":
+    ensure => directory,
+    owner  => $username,
+    group  => $groupname,
+    mode   => '0750',
+  }
+  
+  exec { "SSH key for ${username}":
+    command => "ssh-keygen -C '${username}' -b 2048 -f '${homedir}/.ssh/id_rsa' -t rsa -N ''",
+    creates => "${homedir}/.ssh/id_rsa",
+    path    => $facts['path'],
+  }
+  
 }
